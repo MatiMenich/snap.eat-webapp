49b3cbf447ce298a9591a66fce0fbece5f6f8b69
Matias Menich <matimenich@gmail.com>
2014-08-18 23:34:23 -0400

    [ADD] devise localization and new login page and layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  layout :layout
+
   before_action :set_locale
   before_action :authenticate_user!
 
   # Include these helpers in every controller
   include TranslationsHelper
+  include QrCodeHelper
+
+  private
+
+  def layout
+    # only turn it off for login pages:
+    is_a?(Devise::SessionsController) ? "empty" : "application"
+    # or turn layout off for every devise controller:
+    # devise_controller? && "application"
+  end
+
 end

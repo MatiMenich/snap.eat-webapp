a79dfa59b5473024845bbe633a3c144f530726ea
Matias Menich <matimenich@gmail.com>
2014-08-20 22:50:23 -0400

    [ADD] loading state for order button

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,5 +1,11 @@
 var ready = function() {
     $("#responsive_code").fitText(0.3, { minFontSize: '20px', maxFontSize: '100px' });
+
+
+    $('#order').click(function () {
+	    var btn = $(this)
+	    btn.button('loading')
+	});
 };
 $(document).ready(ready);
 $(document).on('page:load', ready);

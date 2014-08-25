069fe7bbc9ea62cde2e64057fd0d2697f9b05d2e
Matias Menich <matimenich@gmail.com>
2014-08-24 21:50:48 -0400

    [ADD] new product behaviour for new order form

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -6,6 +6,26 @@ var ready = function() {
 	    var btn = $(this)
 	    btn.button('loading')
 	});
+
+	$('#add_product').click(function () {
+		var newInputs = $('.template').html();
+		var removeButton = $('<a class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Eliminar</a>');
+
+		var inputDiv = $('<div class="new_product animated-1 fadeInDown"></div>');
+
+		inputDiv.append(removeButton);
+		inputDiv.append(newInputs);
+		$('.product-fieldset').append(inputDiv);
+
+
+		removeButton.click(function () {
+			inputDiv.removeClass('animated-1 fadeInDown')
+			inputDiv.addClass('animated-05 fadeOutLeft');
+	        setTimeout(function() {
+            	inputDiv.remove();
+          	}, 500);
+		});
+	});
 };
 $(document).ready(ready);
 $(document).on('page:load', ready);
